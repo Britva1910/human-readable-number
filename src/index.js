930fdeca91e1adaa30426e9596cd734a63d5cd7c
mezolit <bbritva1910@gmail.com>
2021-07-28 16:12:12 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let arr = number.toString().split('').map(Number)
+
+    let ones = ["", 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', "ten ", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    let tens = ["", "", 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
+    let hundreds = ["", 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine '];
+
+    if (arr.length > 2) {
+        let one = ones[arr[2]];
+        let ten = tens[arr[1]];
+        let hundred = hundreds[arr[0]];
+        return `${hundred}hundred ${ten}${one}`
+    } else if (arr.length === 1) {
+        let one = ones[arr[1]];
+        let ten = tens[arr[0]];
+        return `${ten}${one}`;
+    } else {
+        let one = ones[arr[1]];
+        return `${one}`;
+    }
+
 }
