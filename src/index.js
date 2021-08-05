a91fe8148f00f1f7f552bfcfc06ccd54d3ee509c
Britva1910 <85491523+Britva1910@users.noreply.github.com>
2021-08-05 15:39:19 +0300

    Update index.js
    add solution

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 module.exports = function toReadable(number) {
-    let arr = number.toString().split('').map(Number)
-
-    let ones = ["", 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', "ten ", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-    let tens = ["", "", 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
-    let hundreds = ["", 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine '];
-
-    if (arr.length > 2) {
-        let one = ones[arr[2]];
-        let ten = tens[arr[1]];
-        let hundred = hundreds[arr[0]];
-        return `${hundred}hundred ${ten}${one}`
-    } else if (arr.length === 1) {
-        let one = ones[arr[1]];
-        let ten = tens[arr[0]];
-        return `${ten}${one}`;
-    } else {
-        let one = ones[arr[1]];
-        return `${one}`;
+    const ones = ['', ' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine', ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const hundreds = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine '];
+    let str = number.toString();
+    if (number === 0) {
+        return 'zero'
     }
+    return convertHundred(number).trim();
 
+    function convertHundred(number) {
+        if (number > 99) {
+            let hundred = Math.floor(number / 100);
+            str = (number % 100).toString();
+            return `${hundreds[hundred]}hundred${convertTens(number%100)}`
+        } else {
+            return convertTens(number);
+        }
+    };
+
+    function convertTens(number) {
+        if (number < 20) {
+            return `${ones[number]}`;
+        } else {
+            return ` ${tens[str[0]]}${ones[str[1]]}`;
+        }
+    };
 }
